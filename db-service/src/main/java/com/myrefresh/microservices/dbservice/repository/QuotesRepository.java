5faf449c9deb24520a8fd6d49804fbebc40e8e57
Padma A. <apadma.ap@gmail.com>
2021-10-05 23:37:08 +0530

    DBService created rest resouce for Quote entity using H2db and made it a Eureka Client

---
+++ db-service/src/main/java/com/myrefresh/microservices/dbservice/repository/QuotesRepository.java
@@ -0,0 +1,10 @@
+package com.myrefresh.microservices.dbservice.repository;
+
+import com.myrefresh.microservices.dbservice.model.Quote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface QuotesRepository extends JpaRepository<Quote, Integer> {
+    List<Quote> findByUserName(String username);
+}
